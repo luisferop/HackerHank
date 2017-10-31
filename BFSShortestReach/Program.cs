f4b0153fd4cdf4e444e506b87341bd57327c5b61
Luís Pereira <luisferop@gmail.com>
2017-10-30 22:34:17 -0200

    Studying Algorithms

---
+++ BFSShortestReach/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFSShortestReach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const long edgeLength = 6;
+            long totalQueries = Convert.ToInt64(Console.ReadLine());
+            string[] results = new string[totalQueries];
+            for (long i = 0; i < totalQueries; i++)
+            {
+                string[] par1 = Console.ReadLine().Split(' ');
+                long lngVertices = Convert.ToInt64(par1[0]);
+                long[] vertices = new long[lngVertices];
+                bool[] isReachable = new bool[lngVertices];
+                for (long j = 0; j < vertices.Length; j++)
+                {
+                    vertices[j] = j+1;
+                }
+                List<Tuple<long, long>> listEdges = new List<Tuple<long, long>>();
+                for (long k = 0; k < Convert.ToInt64(par1[1]); k++)
+                {
+                    string[] strTuple = Console.ReadLine().Split(' ');
+                    listEdges.Add(new Tuple<long, long>(Convert.ToInt64(strTuple[0]), Convert.ToInt64(strTuple[1])));
+                }
+                var graph = new Graph<long>(vertices, listEdges.ToArray());
+
+                var algorithms = new Algorithms();
+
+                var startVertex = Convert.ToInt64(Console.ReadLine());
+                var shortestPath = algorithms.ShortestPathFunction(graph, startVertex);
+                StringBuilder sb = new StringBuilder();
+                for (long v = 0; v < lngVertices; v++)
+                {
+                    if (vertices[v] != startVertex)
+                    {
+                        try
+                        {
+                            var obj = shortestPath(vertices[v]).ToList();
+                            obj.Remove(startVertex);
+
+                            if (obj.Count > 0)
+                            {
+                                sb.Append(String.Format("{0} " ,edgeLength * obj.Count));
+                            }
+                        }
+                        catch
+                        {
+
+                            sb.Append("-1 ");
+                        }
+                    }
+                }
+                results[i] = sb.ToString();
+            }
+            for (long i = 0; i < totalQueries; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
---
+++ JoinToTheMoon/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinToTheMoon
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            var firstLine = Console.ReadLine().Split(' ');
+            var totalPairs = Convert.ToInt64(firstLine[1]);
+            var totalAstronauts = Convert.ToInt64(firstLine[0]);
+
+            if (totalAstronauts == 1) Console.Write(1);
+
+            var visited = new bool[totalAstronauts];
+            var sets = new List<long>();
+
+            var graph = new Dictionary<long, List<long>>();
+
+            for (var i = 0; i < totalAstronauts; i++)
+            {
+                graph.Add(i, new List<long>());
+            }
+
+            for (var i = 0; i < totalPairs; i++)
+            {
+                var pair = Console.ReadLine().Split(' ');
+                var first = Convert.ToInt64(pair[0]);
+                var second = Convert.ToInt64(pair[1]);
+
+                if (first >= totalAstronauts || second >= totalAstronauts) return;
+
+                graph[first].Add(second);
+                graph[second].Add(first);
+            }
+
+            for (var i = 0; i < totalAstronauts; i++)
+            {
+                if (visited[i]) continue;
+                sets.Add(DFS(i, graph, visited));
+            }
+
+            var answer = 0L;
+            var sum = 0L;
+            foreach (var i in sets)
+            {
+                answer += sum * i;
+                sum += i;
+            }
+
+            Console.WriteLine(answer);
+        }
+
+        static long DFS(long item, Dictionary<long, List<long>> graph, bool[] visited)
+        {
+            if (visited[item]) return 0;
+            var count = 1L;
+
+            visited[item] = true;
+            foreach (var i in graph[item])
+            {
+                count += DFS(i, graph, visited);
+            }
+
+            return count;
+        }
+    }
+}
