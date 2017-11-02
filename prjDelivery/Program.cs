472ba6479d68a6b8283200cbb501b9f9ff1fe51f
Luís Pereira <luisferop@gmail.com>
2017-11-02 13:05:20 -0200

    New algorithms

---
+++ prjDelivery/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjDelivery
+{
+    class Program
+    {
+        // Input/Output code
+
+        // The first line of the input contains two decimal integers separated by space:
+        //   - the maximum weight ('W') that can be delivered in a single trip
+        //   - the quantity of packages ('P') to be delivered.
+        // The following 'P' lines contain a decimal integer representing the weight of each package.
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int tripMaxWeight = int.Parse(tokens[0]);
+            int numberOfPackages = int.Parse(tokens[1]);
+
+            int[] packagesWeight = new int[numberOfPackages];
+            for (int i = 0; i < numberOfPackages; i++)
+            {
+                packagesWeight[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(minimumNumberOfTrips(tripMaxWeight, packagesWeight));
+        }
+
+        /*
+   * Complete the function below.
+   */
+
+        static int minimumNumberOfTrips(int tripMaxWeight, int[] packagesWeight)
+        {
+            int totalTrips = 0;
+            Dictionary<int, bool> delivery = new Dictionary<int, bool>();
+            for (int i = 0; i < packagesWeight.Length; i++)
+            {
+                if (!delivery.ContainsKey(packagesWeight[i]))
+                {
+                    delivery.Add(packagesWeight[i], false);
+                }
+            }
+
+            for (int i = 0; i < packagesWeight.Length; i++)
+            {
+                int totalWeightTrip = 0;
+                if (!delivery[packagesWeight[i]])
+                {
+                    totalWeightTrip = packagesWeight[i];
+                    bool insideExecuted = false;
+                    for (int j = i + 1; j < packagesWeight.Length; j++)
+                    {
+                        insideExecuted = true;
+                        if (!delivery[packagesWeight[j]])
+                        {
+                            if (totalWeightTrip + packagesWeight[j] <= tripMaxWeight)
+                            {
+                                delivery[packagesWeight[j]] = true;
+                                delivery[packagesWeight[i]] = true;
+                                totalTrips = totalTrips + 1;
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    if (!insideExecuted)
+                    {
+                        delivery[packagesWeight[i]] = true;
+                        totalTrips = totalTrips + 1;
+                    }
+
+                }
+            }
+            return totalTrips;
+        }
+    }
+}
---
+++ prjDrones/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace prjDrones
+{
+    class Program
+    {
+        private class Drone
+        {
+            public int Id { get; set; }
+            public int FlightRange { get; set; }
+        }
+
+        // Input/Output code
+
+        // The first line of the input contains three decimal ints separated by space:
+        //   - total number of drones ('D')
+        //   - number of drones to be selected ('G')
+        //   - number of drones in maintenance ('M')
+        // The following 'D' lines each contains two decimal ints separated by space with information about each drone:
+        //   - ID
+        //   - flight range in kilometers
+        // The following 'M' lines each contains the numeric ID of a drone currently in maintenance.
+
+        // Print the IDs of the 'G' selected drones to the standard output, one per line, sorted by flight range (greater first).
+        static void Main(String[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int numberOfDrones = int.Parse(tokens[0]);
+            int requiredDrones = int.Parse(tokens[1]);
+            int numberOfDronesInMaintenance = int.Parse(tokens[2]);
+
+            var drones = new List<Drone>();
+            var inMaintenanceDrones = new List<int>();
+
+            for (int i = 0; i < numberOfDrones; i++)
+            {
+                tokens = Console.ReadLine().Split();
+                drones.Add(new Drone { Id = int.Parse(tokens[0]), FlightRange = int.Parse(tokens[1]) });
+            }
+
+            for (int i = 0; i < numberOfDronesInMaintenance; i++)
+            {
+                inMaintenanceDrones.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var result = GreatestFlightRangeDrones(requiredDrones, drones, inMaintenanceDrones);
+
+            foreach (var droneId in result)
+            {
+                Console.WriteLine(droneId);
+            }
+            Console.ReadLine();
+        }
+        /*
+     * Complete the function below.
+     */
+        static List<int> GreatestFlightRangeDrones(int requiredDrones, List<Drone> drones, List<int> inMaintenanceDrones)
+        {
+            List<int> listRequiredDrones = new List<int>();
+            SortedList<int, Drone> dronesSorted = new SortedList<int, Drone>();
+            
+            foreach (Drone item in drones)
+            {
+                bool isAvailable = true;
+                foreach (int inMant in inMaintenanceDrones)
+                {
+                    if (inMant == item.Id)
+                    {
+                        isAvailable = false;
+                        break;
+                    }
+                }
+                if (isAvailable)
+                {
+                    dronesSorted.Add(item.FlightRange, item);
+                }
+            }
+            int count = 0;
+            int[] reverseDrones = new int[dronesSorted.Count];
+            foreach (var stListItem in dronesSorted)
+            {
+                reverseDrones[count] = stListItem.Value.Id;
+                count = count + 1;
+
+            }
+            count = 0;
+            for (int i = reverseDrones.Length-1; i >= 0; i--)
+            {
+                if (count < requiredDrones)
+                {
+                    listRequiredDrones.Add(reverseDrones[i]);
+                }
+                count = count + 1;
+            }
+
+            return listRequiredDrones;
+
+        }
+    }
+}
